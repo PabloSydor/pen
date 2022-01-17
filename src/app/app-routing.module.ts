af949f41b3776dba521f16335074dd32da88dc92
pablo <pablosydor@gmail.com>
2022-01-17 09:34:29 +0100

    cambios clase router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { TrabajadoresComponent } from './trabajadores/trabajadores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
 
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+
+    {path:'', component: InicioComponent},
+    {path:'nuevo', component: NuevoComponent},
+    {path:'trabajadores', component: TrabajadoresComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CardTrabajadorComponent } from './card-trabajador/card-trabajador.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { TrabajadoresComponent } from './trabajadores/trabajadores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardTrabajadorComponent
+    CardTrabajadorComponent,
+    NuevoComponent,
+    TrabajadoresComponent,
+    InicioComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
